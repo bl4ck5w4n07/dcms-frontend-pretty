3100b53818f30fbc18bd195804670f63bdc35f49
Black Swan <irayjb.07@gmail.com>
2025-08-18 16:51:14 +0800

    Fix: next.config.ts allowedDevOrigins

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: [
+    'localhost',
+    '192.168.254.122',        // Your machine’s LAN IP
+    '*.local',             // Example of wildcard for subdomains
+  ],
 };
 
 export default nextConfig;
